975c4e24503081e833725ce0c8ddb9cccee4df8c
Daniel <element11sodium@gmail.com>
2024-11-04 17:50:01 -0800

    up a little

--- boards/shields/nice_view_custom/widgets/util.c
+++ boards/shields/nice_view_custom/widgets/util.c
@@ -30,10 +30,10 @@ void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_rect_dsc_t rect_white_dsc;
     init_rect_dsc(&rect_white_dsc, LVGL_FOREGROUND);
 
-    lv_canvas_draw_rect(canvas, 5, 7, 16, 8, &rect_white_dsc);
-    lv_canvas_draw_rect(canvas, 6, 8, 14, 6, &rect_black_dsc);
-    lv_canvas_draw_rect(canvas, 7, 9, (state->battery + 2) / 8, 4, &rect_white_dsc);
-    lv_canvas_draw_rect(canvas, 21, 9, 2, 4, &rect_white_dsc);
+    lv_canvas_draw_rect(canvas, 5, 5, 16, 8, &rect_white_dsc);
+    lv_canvas_draw_rect(canvas, 6, 6, 14, 6, &rect_black_dsc);
+    lv_canvas_draw_rect(canvas, 7, 6, (state->battery + 2) / 8, 4, &rect_white_dsc);
+    lv_canvas_draw_rect(canvas, 21, 7, 2, 4, &rect_white_dsc);
 
     /*
     if (state->charging) {
